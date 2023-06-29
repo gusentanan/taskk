ed1cf195dae7b62b7086033ae8004f14ff1263c8
Bagus Merta <merta.bagus24@gmail.com>
2023-06-29 15:37:13 +0800

    feat: add taskk app icon

---
+++ app/src/main/java/com/bagusmerta/taskk/presentation/designsystem/icon/TaskkIcon.kt
@@ -0,0 +1,14 @@
+package com.bagusmerta.taskk.presentation.designsystem.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.ArrowBack
+
+/**
+ *  Taskk app Icon: Material icon from Material3 and Custom icon from drawables
+ * */
+object TaskkIcon {
+    val Add = Icons.Rounded.Add
+    val ArrowBack = Icons.Rounded.ArrowBack
+
+}
