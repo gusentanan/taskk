54dad2da395e06eeedad547b1501af2213c97901
Bagus Merta <merta.bagus24@gmail.com>
2023-06-29 15:00:37 +0800

    feat: add app color scheme

--- app/src/main/java/com/bagusmerta/taskk/presentation/designsystem/theme/Color.kt
+++ app/src/main/java/com/bagusmerta/taskk/presentation/designsystem/theme/Color.kt
@@ -2,10 +2,20 @@ package com.bagusmerta.taskk.presentation.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/**
+ *  Light Color Scheme
+ * */
+val lPrimary =  Color(0xFF0067FF)
+val lBackground = Color(0xFFF5F5F5)
+val lItemBackground = Color(0xFFCACACA)
+val lOn = Color(0xFF130E0F)
+val lError = Color(0xFFEA6D7E)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+/**
+ *  Dark Color Scheme
+ * */
+val dPrimary =  Color(0xFF0067FF)
+val dBackground = Color(0xFF130E0F)
+val dItemBackground = Color(0xFF333333)
+val dOn = Color(0xFFF5F5F5)
+val dError = Color(0xFFFF1D3C)
--- app/src/main/java/com/bagusmerta/taskk/presentation/designsystem/theme/Theme.kt
+++ app/src/main/java/com/bagusmerta/taskk/presentation/designsystem/theme/Theme.kt
@@ -10,31 +10,46 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = dPrimary,
+    primaryContainer = dPrimary,
+    secondary = dItemBackground,
+    secondaryContainer = dItemBackground,
+    background = dBackground,
+    onBackground = dOn,
+    onPrimary = Color.Black,
+    onPrimaryContainer = Color.Black,
+    onSecondary = dOn,
+    onSecondaryContainer = dOn,
+    surface = dBackground,
+    onSurface = dOn,
+    error = dError,
+    onError = Color.White,
+
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = lPrimary,
+    primaryContainer = lPrimary,
+    secondary = lItemBackground,
+    secondaryContainer = lItemBackground,
+    background = lBackground,
+    onBackground = lOn,
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onPrimaryContainer = Color.White,
+    onSecondary = lOn,
+    onSecondaryContainer = lOn,
+    surface = lBackground,
+    onSurface = lOn,
+    error = lError,
+    onError = Color.White
+
 )
 
 @Composable
