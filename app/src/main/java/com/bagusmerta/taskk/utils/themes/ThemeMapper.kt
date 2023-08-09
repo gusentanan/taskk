9d0b6453fd5ea3ed3b017e642c1a70345fb49c89
Bagus Merta <merta.bagus24@gmail.com>
2023-08-09 13:34:03 +0800

    feat: add theme mapper and provider

--- app/src/main/java/com/bagusmerta/taskk/utils/themes/ThemeMapper.kt
+++ app/src/main/java/com/bagusmerta/taskk/utils/themes/ThemeMapper.kt
@@ -1,10 +1,21 @@
 package com.bagusmerta.taskk.utils.themes
 
+import com.bagusmerta.taskk.model.preference.ThemePreference
+
+/*
+* Mapper to proto-datastore
+* */
 fun TaskkTheme.toThemePref() =  when (this) {
-    TaskkTheme.SYSTEM -> {}
-    else -> {}
+    TaskkTheme.SYSTEM -> ThemePreference.SYSTEM
+    TaskkTheme.LIGHT -> ThemePreference.LIGHT
+    TaskkTheme.DARK -> ThemePreference.DARK
 }
 
-fun TaskkTheme.toTheme(){
-
+/*
+* Mapper to model/enum classes
+* */
+fun ThemePreference.toTheme() = when (this) {
+    ThemePreference.SYSTEM -> TaskkTheme.SYSTEM
+    ThemePreference.LIGHT -> TaskkTheme.LIGHT
+    ThemePreference.DARK -> TaskkTheme.DARK
 }
--- app/src/main/java/com/bagusmerta/taskk/utils/themes/ThemeProvider.kt
+++ app/src/main/java/com/bagusmerta/taskk/utils/themes/ThemeProvider.kt
@@ -1,9 +1,12 @@
 package com.bagusmerta.taskk.utils.themes
 
 import androidx.datastore.core.DataStore
+import com.bagusmerta.taskk.model.preference.ThemePreference
 import com.bagusmerta.taskk.utils.Dispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -11,7 +14,21 @@ import javax.inject.Named
 
 class ThemeProvider @Inject constructor(
     @Named(Dispatcher.DISPATCHER_IO) private val dispatcher: CoroutineDispatcher,
-    private val taskkThemeDataStore: DataStore<TaskkTheme>,
+    private val taskkThemeDataStore: DataStore<ThemePreference>,
 ) {
+
+    fun getTheme(): Flow<TaskkTheme>{
+        return taskkThemeDataStore.data.map { it.toTheme() }
+            .catch { emit(TaskkTheme.SYSTEM) }
+            .flowOn(dispatcher)
+    }
+
+    suspend fun setTheme(data:TaskkTheme){
+        withContext(dispatcher){
+            taskkThemeDataStore.updateData {
+                data.toThemePref()
+            }
+        }
+    }
 
 }
