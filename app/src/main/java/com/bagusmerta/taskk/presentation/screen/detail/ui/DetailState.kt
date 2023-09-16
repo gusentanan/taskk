0c10b1ac9f7711d0a9f8947927a39d0f1c348bea
Bagus Merta <merta.bagus24@gmail.com>
2023-09-16 20:07:26 +0800

    feat: add detail state and viewmodel

--- app/src/main/java/com/bagusmerta/taskk/presentation/screen/detail/ui/DetailState.kt
+++ app/src/main/java/com/bagusmerta/taskk/presentation/screen/detail/ui/DetailState.kt
@@ -1,4 +1,20 @@
 package com.bagusmerta.taskk.presentation.screen.detail.ui
 
-class DetailState {
+import androidx.compose.ui.text.input.TextFieldValue
+import com.bagusmerta.taskk.domain.model.TaskkToDo
+import com.bagusmerta.taskk.utils.wrapper.DateTimeProvider
+import com.bagusmerta.taskk.utils.wrapper.DateTimeProviderImpl
+import javax.annotation.concurrent.Immutable
+
+@Immutable
+data class DetailState(
+    val editTaskkTitle: TextFieldValue = TextFieldValue(),
+    val editTaskkNote: TextFieldValue = TextFieldValue(),
+    val taskk: TaskkToDo = TaskkToDo(
+        createdAt = DateTimeProviderImpl().getNowDate(),
+        updatedAt = DateTimeProviderImpl().getNowDate()
+    )
+){
+    val validTaskkTitle = editTaskkTitle.text.isNotBlank()
+    val validTaskkNote = editTaskkNote.text.isNotBlank()
 }
--- app/src/main/java/com/bagusmerta/taskk/presentation/screen/detail/ui/DetailViewModel.kt
+++ app/src/main/java/com/bagusmerta/taskk/presentation/screen/detail/ui/DetailViewModel.kt
@@ -1,4 +1,38 @@
 package com.bagusmerta.taskk.presentation.screen.detail.ui
 
-class DetailViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import com.bagusmerta.taskk.navigation.ARG_LIST_ID
+import com.bagusmerta.taskk.navigation.ARG_TASKK_ID
+import com.bagusmerta.taskk.presentation.screen.detail.data.IDetailEnvironment
+import com.bagusmerta.taskk.utils.vmutils.StateViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    detailEnvironment: IDetailEnvironment
+): StateViewModel<DetailState, DetailEffect, DetailEvent, IDetailEnvironment>(DetailState(), detailEnvironment){
+
+    val taskkId = savedStateHandle.get<String>(ARG_TASKK_ID)
+    val listId = savedStateHandle.get<String>(ARG_LIST_ID)
+    init {
+        viewModelScope.launch {
+            if(taskkId != null && listId != null){
+                environment.getTaskkById(taskkId)
+                    .collect {
+                        setState {
+                            copy(taskk = taskk)
+                        }
+                    }
+            }
+        }
+    }
+
+    override fun dispatch(event: DetailEvent) {
+        TODO("Not yet implemented")
+    }
+
 }
